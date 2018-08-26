e20268e304b0882e413ac5a0852a2a03866eb2c7
Anurag Mishra <mishra191@gmail.com>
2018-08-27 00:30:42 +0530

    Update sigmoid.m

--- machine-learning/sigmoid.m
+++ machine-learning/sigmoid.m
@@ -1,6 +1,7 @@
 function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
+% There could be another function can be used here
 
 g = 1.0 ./ (1.0 + exp(-z));
 end
